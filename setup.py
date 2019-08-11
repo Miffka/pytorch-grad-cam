a405d97f3c67eadfb391a1b2ee6bcbd440e4fa87
Miffka <miffka1992@gmail.com>
2019-08-11 11:18:08 +0300

    packed to package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='gradcam',
+    packages=find_packages(),
+    version='0.1.0',
+    description='PyTorch implementation of GradCAM',
+    author='Jacob Gildenblat',
+    license='',
+)
